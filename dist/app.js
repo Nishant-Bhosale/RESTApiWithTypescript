b1d1d88007374a21599c17924c28f9ef06f8ad92
Nishant <nishantbhosale244@gmail.com>
2021-08-14 09:54:28 +0530

    Added basic crud routes

--- dist/app.js
+++ dist/app.js
@@ -4,5 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const projects_1 = __importDefault(require("./routes/projects"));
 const app = express_1.default();
+app.use("/projects", projects_1.default);
+app.use((err, req, res, next) => {
+    res.status(500).json({ msg: err.message });
+});
 app.listen(3000);
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import projectRoutes from "./routes/projects";
 
 const app = express();
+
+app.use("/projects", projectRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	res.status(500).json({ msg: err.message });
+});
 
 app.listen(3000);
